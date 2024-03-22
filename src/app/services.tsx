b75dd77c0031260bc4725058f710b41324894c7b
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-03-22 20:49:33 -0300

    Update services.tsx

--- src/app/services.tsx
+++ src/app/services.tsx
@@ -60,16 +60,16 @@ export function Services() {
       className="flex flex-col items-center justify-center gap-16 md:gap-32"
     >
       <div className="relative flex flex-col gap-16 md:gap-32 max-w-7xl">
-        <div className="bg-primary w-full max-w-[200px] md:max-w-[500px] h-8"></div>
+        <div className="bg-primary w-full max-w-[200px] md:max-w-[500px] h-4 md:h8"></div>
         <h1
           className={cn(
             layout.fonts.highlight.className,
-            'text-2xl md:text-4xl font-bold uppercase -mt-16',
+            'text-2xl md:text-4xl font-bold uppercase -mt-8 md:-mt-16',
           )}
         >
           Qual sua história?
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-16 w-full ">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-16 w-full ">
           {services.map((service, index) => (
             <Link
               href={`https://wa.me/${profile.phone}?text=${service.message}`}
