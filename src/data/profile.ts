c252031a32c70a670b0309ced86486682d5ab069
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-03-25 20:43:44 -0300

    Update profile.ts

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,5 +1,5 @@
 import { ProfileType } from '@/model/profile'
-import { FaFacebook, FaInstagram } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 import logoImg from '../../public/logo.svg'
 
 export const profile: ProfileType = {
@@ -11,14 +11,9 @@ export const profile: ProfileType = {
     'Dedicando minha carreira à advocacia, sou um profissional comprometido em oferecer soluções jurídicas eficazes e personalizadas aos meus clientes. Com uma abordagem especializada para o seu caso, busco entender todas as suas necessidades e oferecer uma orientação legal sólida e acessível. Estou aqui para ajudá-lo juridicamente com confiança e tranquilidade. Sinta-se à vontade para entrar em contato e agendar uma consulta para discutir suas preocupações legais. O seu caso é a minha prioridade.',
   social: [
     {
-      name: 'Facebook',
-      icon: FaFacebook,
-      href: 'https://facebook.com/advogado',
-    },
-    {
       name: 'Instagram',
       icon: FaInstagram,
-      href: 'https://instagram.com/advogado',
+      href: 'https://instagram.com/caioramaoadv',
     },
   ],
 }
