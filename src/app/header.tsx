07707f168414db7b25c70fcb9055cc2567f048eb
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-03-27 18:55:28 -0300

    Update header.tsx

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,22 +1,19 @@
-import { Nav } from '@/components/nav'
-import { navItems } from '@/data/nav-links'
 import { profile } from '@/data/profile'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex flex-row items-center justify-center p-4 backdrop-blur-sm">
       <div className="flex flex-row items-center justify-between w-full max-w-7xl">
-        <div className="flex flex-row items-center gap-2">
+        <Link href="/" className="flex flex-row items-center gap-2">
           <Image
             src={profile.logo}
             alt={profile.name}
             width={263}
             height={40}
           />
-        </div>
-
-        <Nav items={navItems} />
+        </Link>
       </div>
     </header>
   )
